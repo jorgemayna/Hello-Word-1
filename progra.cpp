b91c969d593d855d85b4875f649d9a5b556d92e1
utec <jorgemayna@gmail.com>
2019-03-28 12:23:29 -0500

    vocales

--- progra.cpp
+++ progra.cpp
@@ -5,12 +5,18 @@ using namespace std;
 int main(){
 	string 	palabra;
 	set<char> gg;
+	set<char> vocales;
+	vocales.insert('a');
+	vocales.insert('e');
+	vocales.insert('i');
+	vocales.insert('o');
+	vocales.insert('u');
 	int cont=0;
 	cout<<"Escribe una palabra: ";
-	cin>> palabra;
+	cin >> palabra;
 	for(int i=0;i<palabra.length();i++){
 		char le=palabra[i];
-		if (gg.count(le)!=0){	
+		if (gg.count(le)==0 && vocales.count(le)!=0){	
 		for(int j=i;j<palabra.length();j++){
 			if(le==palabra[j]){
 				cont++;
